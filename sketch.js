20df1a7cef3d7a24a5c0624c309a465156799ae1
Shade Rickard <edmond.rickard@gmail.com>
2025-11-10 19:12:46 +0000

    initial setup done

--- sketch.js
+++ sketch.js
@@ -1,16 +1,60 @@
+let held_item = null;
+
 function setup() {
   window_x = (windowWidth);
   window_y = (windowWidth*0.5625);
   createCanvas(window_x, window_y);
 }
 
+
 function draw() {
   background(150);
-  container_width = window_y/14
-  square(container_width, container_width, 3*container_width)
-  square(container_width, 5*container_width, 3*container_width)
-  square(container_width, 9*container_width, 3*container_width)
-  square(21*container_width, container_width, 3*container_width)
-  square(21*container_width, 5*container_width, 3*container_width)
-  square(21*container_width, 9*container_width, 3*container_width)
+  chunk = window_x/25 //a chunk is a workable unit of measurment for colliders
+  fill(255,255,255);
+  square(chunk, chunk, 3*chunk)
+  square(chunk, 5*chunk, 3*chunk)
+  square(chunk, 9*chunk, 3*chunk)
+  square(21*chunk, chunk, 3*chunk)
+  square(21*chunk, 5*chunk, 3*chunk)
+  square(21*chunk, 9*chunk, 3*chunk)
+
+  rect(10*chunk, 5*chunk,5*chunk,8*chunk)
+
+  fill(225,0,225);
+  square(7*chunk, 5*chunk, chunk)
+  text(held_item,100,100)
+}
+
+function mouseClicked() {
+  if (mouseX > chunk && mouseX < 4*chunk) { //fresh ingredient
+    if (mouseX > chunk && mouseX < 4*chunk) { //cup
+      held_item = ("fresh_cup")
+}
+    }
+    else if (mouseX > chunk && mouseX < 4*chunk) { //milk
+      held_item = ("fresh_milk")
+    }
+    else if (mouseX > chunk && mouseX < 4*chunk) { //boba
+      held_item = ("fresh_boba")
+    }
+  }
+  if (mouseX > 21*chunk && mouseX < 24*chunk) { //old ingredient
+    if (mouseX > chunk && mouseX < 4*chunk) { //cup
+      held_item = ("old_cup")
+    }
+    else if (mouseX > chunk && mouseX < 4*chunk) { //milk
+      held_item = ("old_milk")
+    }
+    else if (mouseX > chunk && mouseX < 4*chunk) { //boba
+      held_item = ("old_boba")
+    }
+  }
+}
+
+function mouseReleased() {
+  if (mouseX>10*chunk && mouseX<15*chunk && mouseY>5*chunk && mouseY<13*chunk){
+  }
+  else{
+    held_item=null;
+  }
 }
