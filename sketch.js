467aacbeec6e22b87fa81ed56e479bc8ba4fde46
Shade Rickard <edmond.rickard@gmail.com>
2025-11-10 20:00:36 +0000

    cup booleans work

--- sketch.js
+++ sketch.js
@@ -27,7 +27,9 @@ function draw() {
   square(7*chunk, 5*chunk, chunk)
 
   text(held_item,100,100)
-
+  text(prep_cup,100,120)
+  text(prep_milk,100,140)
+  text(prep_boba,100,160)
 }
 
 function mousePressed() {
@@ -59,9 +61,16 @@ function mouseReleased() {
   if (mouseX>10*chunk && mouseX<15*chunk && mouseY>5*chunk && mouseY<13*chunk){ //mouse released on prep station
     if (held_item != "old_cup" && held_item != "fresh_cup" && prep_cup==false){
       held_item=("null"); //nothing will happen if you try to use milk or boba without a cup
-      held_item = "trigger"
+    } 
+    else if ((held_item == "old_cup" || held_item == "fresh_cup")&&prep_cup==false){
+      prep_cup=true
     }
-
+    else if ((held_item == "old_milk" || held_item == "fresh_milk")&&prep_milk==false){
+      prep_milk=true
+    }
+    else if ((held_item == "old_boba" || held_item == "fresh_boba")&&prep_boba==false){
+      prep_boba=true
+    }
   }
   else{
     held_item=("null"); //mouse not released on prep station
